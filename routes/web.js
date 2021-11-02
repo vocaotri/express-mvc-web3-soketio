b3bf48ce264d4ae4ffc9a06cbef9139d3d2833ae
tri vo <v_tri@gmail.com>
2021-11-02 20:08:09 +0700

    add middleware

--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,7 @@
+const { isUser } = require("../app/middleware/IsUser");
 const homeController = require("../app/controller/HomeController");
 module.exports = async function (config) {
-    let app = config.app;
-    let abi = config.abi;
-    app.get("/", homeController.home(abi, process.env.ADRSM));
-  };
+  let app = config.app;
+  let abi = config.abi;
+  app.get("/", isUser, homeController.home(abi, process.env.ADRSM));
+};
